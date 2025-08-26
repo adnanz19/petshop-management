c66c55924955c5bc939ce2cf25296f475b975f09
Bima Adnandita <bima.adnandita@gmail.com>
2025-08-26 09:06:57 +0700

    add firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA7Ms9W0VVdPrHTCdFs3mpcWCdeTtYXtcg",
+  authDomain: "petshop-manage.firebaseapp.com",
+  projectId: "petshop-manage",
+  storageBucket: "petshop-manage.firebasestorage.app",
+  messagingSenderId: "62849885728",
+  appId: "1:62849885728:web:7d2f051f92e750d2ed3e45"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
